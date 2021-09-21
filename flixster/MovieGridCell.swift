00aade4d51662eb098b3e30d57148ded1b5c5121
Pot_Lid <p8032336@gmail.com>
2021-09-20 22:08:25 -0400

    unit2 done

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by JunWoo Park on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
